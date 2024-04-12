1dc15b6ef68f68ba358a32501802142967f6494b
Ivan Matkov <ivan.matkov@jetbrains.com>
2024-04-12 17:18:50 +0200

    Update usage of `viewModel` in common

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,9 +1,10 @@
 import androidx.compose.runtime.Composable
 import com.matkovivan.nav_cupcake.CupcakeApp
 import com.matkovivan.nav_cupcake.ui.theme.CupcakeTheme
+import com.matkovivan.nav_cupcake.withViewModelStoreOwner
 
 @Composable
-fun App() {
+fun App() = withViewModelStoreOwner {
     CupcakeTheme {
         CupcakeApp()
     }
