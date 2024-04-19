a843f07b9d946bdf4bb99dd724bf6da753f7a019
Ivan Matkov <ivan.matkov@jetbrains.com>
2024-04-19 19:29:07 +0200

    Update version and remove workarounds

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,10 +1,9 @@
 import androidx.compose.runtime.Composable
 import com.matkovivan.nav_cupcake.CupcakeApp
 import com.matkovivan.nav_cupcake.ui.theme.CupcakeTheme
-import com.matkovivan.nav_cupcake.withViewModelStoreOwner
 
 @Composable
-fun App() = withViewModelStoreOwner {
+fun App() {
     CupcakeTheme {
         CupcakeApp()
     }
